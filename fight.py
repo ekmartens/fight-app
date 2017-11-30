0716cde671f06c4393cc52267f2cf6630b95fba1
ekmartens <ekmrtns@gmail.com>
2017-11-29 20:20:34 -0800

    initial commit

---
+++ fight.py
@@ -0,0 +1,11 @@
+#fight moves by type
+
+attack = ['Cut', 'Thrust', 'Swipe', 'Lunge', 'Lunge & Redouble']
+
+attack_area = ['Head Right', 'Head Left', 'Body Right', 'Body Left']
+
+parry = []
+
+fancy = []
+
+ending = []
